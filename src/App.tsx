a8e7a2be03215b599bfd83883cb75aa091e7a9dd
Potato <poltoradnevone@gmail.com>
2023-09-10 19:42:43 +0400

    feat: euler "almost" works

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
 
 import { Button } from "./components/ui/button";
 import { G, M, EARTH_MASS } from "@/lib/constants";
@@ -14,6 +14,7 @@ const firstPlanet: Planet = {
   m: M,
   energy: 0,
 };
+
 // Earth
 const secondPlanet: Planet = {
   x: 149600000000,
@@ -24,11 +25,36 @@ const secondPlanet: Planet = {
   energy: 0,
 };
 
-const App = () => {
-  const [planets, setPlanets] = useState([firstPlanet, secondPlanet]);
-  const [t, setT] = useState(1000 * 60 * 60 * 24);
+const thirdPlanent: Planet = {
+  x: 149600000000 * 2,
+  y: 0,
+  vx: 0,
+  vy: Math.sqrt((G * M) / 149600000000 / 2),
+  m: EARTH_MASS * 2,
+  energy: 0,
+};
 
-  const solveEuler = () => {
+const fourthPlanet: Planet = {
+  x: 149600000000 * 3,
+  y: 0,
+  vx: 0,
+  vy: Math.sqrt((G * M) / 149600000000 / 3),
+  m: EARTH_MASS * 3,
+  energy: 0,
+};
+
+const App = () => {
+  const [planets, setPlanets] = useState<Array<Planet>>([
+    firstPlanet,
+    secondPlanet,
+    thirdPlanent,
+    fourthPlanet,
+  ]);
+  const [t, setT] = useState(360000);
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const solveEuler = useCallback(() => {
     const updatedPlanets = [...planets];
     planets.forEach((planet, index, arr) => {
       const { x, y, vx, vy } = planet;
@@ -52,8 +78,53 @@ const App = () => {
       };
     });
     setPlanets(updatedPlanets);
-  };
-  console.log(planets);
+  }, [planets, t]);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const context = canvasRef.current.getContext("2d");
+      const centerX = 200;
+      const centerY = 200;
+      const radius = 20;
+
+      if (context) {
+        planets.forEach((planet) => {
+          const { x, y } = planet;
+          context.beginPath();
+          console.log(
+            "planent x",
+            planet,
+            centerX + (x * 400) / planets[planets.length - 1].x / 2
+          );
+          console.log(
+            "planent y",
+            planet,
+            centerY + (y * 400) / planets[planets.length - 1].x / 2
+          );
+          context.arc(
+            centerX + (x * 400) / planets[planets.length - 1].x / 2,
+            centerY + (y * 400) / planets[planets.length - 1].x / 2,
+            radius,
+            0,
+            2 * Math.PI,
+            false
+          );
+          context.fillStyle = "yellow";
+          context.fill();
+          context.lineWidth = 5;
+          context.strokeStyle = "orange";
+          context.stroke();
+        });
+      }
+    }
+  }, [canvasRef, planets]);
+
+  // console.log(planets);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => solveEuler(), 100);
+  //   return () => clearInterval(interval);
+  // }, [canvasRef, solveEuler]);
 
   return (
     <div>
@@ -62,6 +133,7 @@ const App = () => {
       <Button className="m-4" onClick={solveEuler}>
         Solve
       </Button>
+      <canvas ref={canvasRef} width={400} height={400}></canvas>
     </div>
   );
 };
