da292ed6e05f7dd1b5af81396e430694216f2343
Potato <poltoradnevone@gmail.com>
2023-09-13 10:08:07 +0400

    feat: Euler done

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,36 @@
 import { useState, useRef, useEffect, useCallback } from "react";
 
-import { Button } from "./components/ui/button";
-import { G, M, EARTH_MASS } from "@/lib/constants";
+import { G, M, EARTH_MASS, EARTH_DISTANCE_FROM_SUN } from "@/lib/constants";
 import type { Planet } from "@/lib/types";
 
-/* Initialization */
-// Sun
-const firstPlanet: Planet = {
-  x: 0,
-  y: 0,
-  vx: 0,
-  vy: 0,
-  m: M,
-  energy: 0,
-};
-
-// Earth
-const secondPlanet: Planet = {
-  x: 149600000000,
-  y: 0,
-  vx: 0,
-  vy: Math.sqrt((G * M) / 149600000000),
-  m: EARTH_MASS,
-  energy: 0,
-};
-
-const thirdPlanent: Planet = {
-  x: 149600000000 * 2,
-  y: 0,
-  vx: 0,
-  vy: Math.sqrt((G * M) / 149600000000 / 2),
-  m: EARTH_MASS * 2,
-  energy: 0,
-};
-
-const fourthPlanet: Planet = {
-  x: 149600000000 * 3,
-  y: 0,
-  vx: 0,
-  vy: Math.sqrt((G * M) / 149600000000 / 3),
-  m: EARTH_MASS * 3,
-  energy: 0,
-};
-
 const App = () => {
-  const [planets, setPlanets] = useState<Array<Planet>>([
-    firstPlanet,
-    secondPlanet,
-    thirdPlanent,
-    fourthPlanet,
-  ]);
-  const [t, setT] = useState(360000);
+  const [planets, setPlanets] = useState<Array<Planet>>(
+    new Array(3).fill(0).map((_, i) => ({
+      x: EARTH_DISTANCE_FROM_SUN * i,
+      y: 0,
+      vx: 0,
+      vy: i === 0 ? 0 : Math.sqrt((G * M) / EARTH_DISTANCE_FROM_SUN / i),
+      m: i === 0 ? M : EARTH_MASS * i,
+      energy: 0,
+    }))
+  );
+  const [canvasDimensions, setCanvasDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const [planetsAmount, setPlanetsAmount] = useState(3);
+
+  const [t, setT] = useState(60 * 60 * 24 * 7);
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const solveEuler = useCallback(() => {
     const updatedPlanets = [...planets];
-    planets.forEach((planet, index, arr) => {
+    planets.forEach((planet, index) => {
       const { x, y, vx, vy } = planet;
       let aX = 0,
         aY = 0;
-      arr.forEach((p, j) => {
+      planets.forEach((p, j) => {
         if (j !== index) {
           const R = Math.sqrt((x - p.x) ** 2 + (y - p.y) ** 2);
           aX += (G * p.m * (p.x - x)) / R ** 3;
@@ -81,59 +51,71 @@ const App = () => {
   }, [planets, t]);
 
   useEffect(() => {
+    setPlanets(() => {
+      return new Array(planetsAmount).fill(0).map((_, i) => ({
+        x: EARTH_DISTANCE_FROM_SUN * i,
+        y: 0,
+        vx: 0,
+        vy: i === 0 ? 0 : Math.sqrt((G * M) / EARTH_DISTANCE_FROM_SUN / i),
+        m: i === 0 ? M : EARTH_MASS * i,
+        energy: 0,
+      }));
+    });
     if (canvasRef.current) {
       const context = canvasRef.current.getContext("2d");
-      const centerX = 200;
-      const centerY = 200;
+      context?.clearRect(0, 0, canvasDimensions.width, canvasDimensions.height);
+    }
+  }, [planetsAmount, canvasRef, canvasDimensions]);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const context = canvasRef.current.getContext("2d");
+      const centerX = canvasDimensions.width / 2;
+      const centerY = canvasDimensions.height / 2;
       const radius = 20;
 
       if (context) {
-        planets.forEach((planet) => {
-          const { x, y } = planet;
-          context.beginPath();
-          console.log(
-            "planent x",
-            planet,
-            centerX + (x * 400) / planets[planets.length - 1].x / 2
-          );
-          console.log(
-            "planent y",
-            planet,
-            centerY + (y * 400) / planets[planets.length - 1].x / 2
-          );
-          context.arc(
-            centerX + (x * 400) / planets[planets.length - 1].x / 2,
-            centerY + (y * 400) / planets[planets.length - 1].x / 2,
-            radius,
-            0,
-            2 * Math.PI,
-            false
-          );
-          context.fillStyle = "yellow";
-          context.fill();
-          context.lineWidth = 5;
-          context.strokeStyle = "orange";
-          context.stroke();
+        planets.forEach((planet, i) => {
+          requestAnimationFrame(() => {
+            const { x, y } = planet;
+            context.beginPath();
+            context.arc(
+              centerX +
+                (x * canvasDimensions.width) /
+                  EARTH_DISTANCE_FROM_SUN /
+                  (planetsAmount * 3),
+              centerY +
+                (y * canvasDimensions.height) /
+                  EARTH_DISTANCE_FROM_SUN /
+                  (planetsAmount * 3),
+              i === 0 ? radius * 2 : radius,
+              0,
+              2 * Math.PI,
+              false
+            );
+            context.fillStyle = "yellow";
+            context.fill();
+            context.lineWidth = 5;
+            context.strokeStyle = "orange";
+            context.stroke();
+          });
         });
       }
     }
-  }, [canvasRef, planets]);
+  }, [canvasRef, planets, planetsAmount, canvasDimensions]);
 
-  // console.log(planets);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => solveEuler(), 100);
-  //   return () => clearInterval(interval);
-  // }, [canvasRef, solveEuler]);
+  useEffect(() => {
+    const interval = setInterval(() => solveEuler(), 33);
+    return () => clearInterval(interval);
+  }, [canvasRef, solveEuler]);
 
   return (
-    <div>
-      to be continued...
-      <pre>{JSON.stringify(planets, null, 4)}</pre>
-      <Button className="m-4" onClick={solveEuler}>
-        Solve
-      </Button>
-      <canvas ref={canvasRef} width={400} height={400}></canvas>
+    <div className="flex items-center justify-center">
+      <canvas
+        ref={canvasRef}
+        width={canvasDimensions.width}
+        height={canvasDimensions.height}
+      ></canvas>
     </div>
   );
 };
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const G = 6.67 * 10 ** -11;
 export const M = 1.98892 * 10 ** 30;
-// export const M = 1.2 * 10 ** 30;
 export const EARTH_MASS = 6 * 10 ** 24;
+export const EARTH_DISTANCE_FROM_SUN = 149600000000;
