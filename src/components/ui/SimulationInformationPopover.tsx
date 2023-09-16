079a27bee2fc285b7fc3dce4512a301bc1e6f692
Potato <poltoradnevone@gmail.com>
2023-09-16 19:48:32 +0400

    feat: made simulation info draggable

--- src/components/ui/SimulationInformationPopover.tsx
+++ src/components/ui/SimulationInformationPopover.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import {
   Card,
   CardContent,
@@ -5,8 +6,8 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Progress } from "@/components/ui/progress";
+} from "@/components/ui/Card";
+import { Progress } from "@/components/ui/Progress";
 import { X } from "lucide-react";
 
 import { usePlanetsContext, useSimulationInformationContext } from "@/contexts";
@@ -27,28 +28,30 @@ export const SimulationInformationPopover: React.FC<InfoPopoverProps> = ({
     useSimulationInformationContext();
 
   return isSimulationInformationOpen ? (
-    <Card className="absolute z-10 top-1/2 left-1/2 pr-8 min-w-[550px] translate-x-[-50%] translate-y-[-50%]">
-      <X
-        onClick={closeSimulationInformation}
-        className="absolute top-4 right-4 text-white cursor-pointer"
-      />
-      <CardHeader>
-        <CardTitle className="text-4xl">Current Session</CardTitle>
-        <CardDescription>Simulation Information</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Total energy: {totalEnergy} J</p>
-      </CardContent>
-      <CardContent>
-        <p>Center of mass vX: {vX}</p>
-      </CardContent>
-      <CardContent>
-        <p>Center of mass vY: {vY}</p>
-      </CardContent>
-      <CardFooter className="flex flex-col items-start gap-4">
-        <p>Elapsed time: {elapsedTime / 60 / 60 / 24} days.</p>
-        <Progress value={(elapsedTime / simulationDuration) * 100} />
-      </CardFooter>
-    </Card>
+    <motion.div drag className="absolute z-20 cursor-grab">
+      <Card className="pr-8 min-w-[550px]">
+        <X
+          onClick={closeSimulationInformation}
+          className="absolute top-4 right-4 text-white cursor-pointer"
+        />
+        <CardHeader>
+          <CardTitle className="text-4xl">Current Session</CardTitle>
+          <CardDescription>Simulation Information</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Total energy: {totalEnergy} J</p>
+        </CardContent>
+        <CardContent>
+          <p>Center of mass vX: {vX}</p>
+        </CardContent>
+        <CardContent>
+          <p>Center of mass vY: {vY}</p>
+        </CardContent>
+        <CardFooter className="flex flex-col items-start gap-4">
+          <p>Elapsed time: {elapsedTime / 60 / 60 / 24} days.</p>
+          <Progress value={(elapsedTime / simulationDuration) * 100} />
+        </CardFooter>
+      </Card>
+    </motion.div>
   ) : null;
 };
